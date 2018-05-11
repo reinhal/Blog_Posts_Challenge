b30695984f7c4b04a4f776768692ab72fba2e24e
Lisa Reinhardt <reinal@gmail.com>
2018-05-11 17:30:18 -0500

    Merge branch 'master' of https://github.com/reinhal/jusnoshin updated PUT test

--- test/test-blog.js
+++ test/test-blog.js
@@ -46,28 +46,22 @@ describe('BlogPosts', function() {
 	});
 
 	it('should update posts on PUT', function() {
-		const updatePost = {
-			title: 'Spicy Jalapeno',
-			content: 'Filet mignon biltong andouille pork chop pig t-bone, labore salami non esse. Ex fugiat nisi brisket, ball tip adipisicing esse rump dolor ea enim hamburger aute salami. Officia lorem doner rump ut drumstick exercitation do pancetta kevin enim. Brisket buffalo qui et.',
-			author: 'Aute Laboris',
-			publishDate: "May, 7, 2018"
-		};
 
 		return chai.request(app)
 			.get('/blog-posts')
 			.then(function(res) {
-				updatePost.id = res.body[0].id;
+				const updatedPost =Object.assign(res.body[0], {
+					title: 'Spicy Jalapeno',
+					content: 'Filet mignon biltong andouille pork chop pig t-bone, labore salami non esse. Ex fugiat nisi brisket, ball tip adipisicing esse rump dolor ea enim hamburger aute salami. Officia lorem doner rump ut drumstick exercitation do pancetta kevin enim. Brisket buffalo qui et.',	
+				});
 				return chai.request(app)
-				.put(`/blog-posts/${updatePost.id}`)
-				.send(updatePost);
-			})
-			.then(function(res) {
-				expect(res).to.have.status(200);
-        		expect(res).to.be.json;
-        		expect(res.body).to.be.a('object');
-        		expect(res.body).to.deep.equal(updatePost);
-			});
-	});
+				.put(`/blog-posts/${res.body[0].id}`)
+				.send(updatedPost)
+				.then(function(res) {
+					expect(res).to.have.status(204);
+				});
+		  });
+	  });
 
 	it('should delete posts on DELETE', function() {
 		return chai.request(app)
